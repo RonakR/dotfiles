ef8b6a11f208e855771e0358812f80a28e9aef6b
Ronak Raithatha <ron.raithatha@blueacorn.com>
2014-03-04 09:31:54 -0500

    added touch and sudo aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,13 @@
 #Quick alias to configure 
-alias zshcon ='gedit ~/.zshrc'
+alias zshcon='gedit ~/.zshrc'
 
 #Quick restart of the shell, without having to close it.
 alias reload!='. ~/.zshrc'
 
 #Simple, but doesn't have autocomplete. Useful for when you know what you want.
-alias install ='sudo apt-get install'
-alias update = 'sudo apt-get update'
+alias install='sudo apt-get install'
+alias update='sudo apt-get update'
+
+#Random shortcuts
+alias t='touch'
+alias s='sudo'
