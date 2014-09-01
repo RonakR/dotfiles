17f302f26d31d2bbbcd4da60aa447c1fbfe904c2
Ronak Raithatha <ron.raithatha@blueacorn.com>
2014-09-01 14:11:38 -0400

    added some more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,3 +11,14 @@ alias update='sudo apt-get update'
 #Random shortcuts
 alias t='touch'
 alias s='sudo'
+
+
+alias gsl='gst | less'
+alias n98='php /home/ronak/n98-magerun.phar'
+alias passpharse='echo Sperm Whales Are Bigger Than Sperm'
+alias cool='echo cool cool cool'
+alias q='clear-cache'
+alias qw='clear-cache;reindex-site'
+alias setXDebug='export XDEBUG_CONFIG="idekey=phpstorm"'
+alias unsetXDebug='unset XDEBUG_CONFIG'
+alias gc='git checkout'
